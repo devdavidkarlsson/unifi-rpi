cd4f5dcf3be611955be91f6d1a1eab50fc730404
devdavidkarlsson <david@inkorgen.nu>
2019-12-28 14:39:33 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "cleanup and start controller"
-/usr/local/bin/docker-compose run mongo rm /data/db/mongod.lock  
-/usr/local/bin/docker-compose up
+/usr/local/bin/docker-compose -f /home/pirate/unifi/docker-compose.yml run mongo rm /data/db/mongod.lock  
+/usr/local/bin/docker-compose -f /home/pirate/unifi/docker-compose.yml up
